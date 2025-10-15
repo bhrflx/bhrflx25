20256eec0d2c75a92fadff6ef617df776ba05fae
Felix Bahr <felix.bahr@kabelmail.de>
2025-10-15 15:02:40 +0200

    feat(page): Initial setup and start building homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontshare(),
+        name: "JetBrains Mono",
+        cssVariable: "--jetbrains-mono",
+        // Download only font files for characters used on the page
+        subsets: ["latin"],
+        // Use a fallback font family matching the intended appearance
+        fallbacks: ["monospace"],
+      },
+    ],
+  },
+});
